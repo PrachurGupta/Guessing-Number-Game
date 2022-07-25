dbc1c70030a7c4081a575c5703cf992aa93be84f
PrachurGupta <prachur12@gmail.com>
2022-07-25 22:21:02 +0530

    Again Button

--- script.js
+++ script.js
@@ -6,10 +6,11 @@ document.querySelector('.message').textContent = '✨ Correct Number!';
 document.querySelector('.number').textContent = 13;
 document.querySelector('.score').textContent = 10;
 
+
 document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
 */
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 
 document.querySelector('.number').textContent = secretNumber;
@@ -25,6 +26,7 @@ document.querySelector('.check').addEventListener('click', function () {
     // Wins
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = '✨ Correct Number!';
+    document.querySelector('.number').textContent = secretNumber;
 
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
@@ -37,6 +39,7 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.score').textContent = score;
     } else {
       document.querySelector('.message').textContent = '💥 You Lost!';
+      document.querySelector('body').style.backgroundColor = 'red';
       document.querySelector('.score').textContent = 0;
     }
 
@@ -48,7 +51,21 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.score').textContent = score;
     } else {
       document.querySelector('.message').textContent = '💥 You Lost!';
+      document.querySelector('body').style.backgroundColor = 'red';
       document.querySelector('.score').textContent = 0;
     }
   }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+  document.querySelector('.message').textContent = 'Start guessing...';
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
+
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
 });
